0ece47d7d628fc5ae4db81715590a8dafdb050ca
Jacqui De Sa <jdesa@mit.edu>
2015-05-05 23:38:14 -0400

    Add Party Menu: Added Styling for Placeholder Text for Inputs

--- js/main.js
+++ js/main.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
     //localStorage['one'] = '1';
     var foo = localStorage['one'];
     console.log(foo);
+
+    $("#partyTime").click(function(){
+  	var a = document.getElementById("partyTime");
+  	a.type="time";  
+  	});
+
+  	$("#partyDate").click(function(){
+  	var a = document.getElementById("partyTime");
+  	a.type="date";  
+  	});
+
+  	$("#partyDate").click(function(){
+  	var a = document.getElementById("partyTime");
+  	a.type="number";  
+  	});
+
 });
